11cdd235060286d5914a14ebce01696911928e78
robertrinh <robert.trinh@yahoo.com>
2024-01-10 16:28:31 +0100

    thread created. check mutex locks

--- philo/src/main.c
+++ philo/src/main.c
@@ -6,7 +6,7 @@
 /*   By: qtrinh <qtrinh@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/11/15 16:45:00 by qtrinh        #+#    #+#                 */
-/*   Updated: 2024/01/04 16:57:39 by qtrinh        ########   odam.nl         */
+/*   Updated: 2024/01/10 16:24:12 by robertrinh    ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,18 +24,79 @@ static int	error_message(void)
 	return(EXIT_FAILURE);
 }
 
+void	monitor(t_data *data)
+{
+	int	i;
+
+	while (1)
+	{
+		i = 0;
+		while (i < data->philo_n)
+		{
+			//check death + eat count
+			// return
+			
+			i++;
+		}
+		usleep(1000);
+	}
+}
+
+static bool	threads_join(t_data *data, int last_thread)
+{
+	int	i;
+
+	i = 0;
+	while (i < last_thread)
+	{
+		if (pthread_join(data->philo[i].id, NULL) != 0)
+		{
+			write(2, "error joining threads\n", 23);
+			return (false);
+		}
+		i++;
+	}
+	return (true);
+}
+
+void	*routine(void *philo)
+{
+	t_philo	*p_struct;
+
+	p_struct = (t_philo *)philo;
+	if (p_struct->id % 2) //if p_id is odd
+		usleep(1000); //wait for a period of time
+	//while (check stop) -> eat, sleep, think
+
+	return (NULL);
+}
+
+static bool	create_threads(t_data *data, int threads)
+{
+	while (threads < data->philo_n)
+	{
+		if (pthread_create(&data->philo[threads].id, NULL, &routine, &data->philo[threads]) != 0)
+		{
+			threads_join(data, threads);
+			//free(data)
+			write(2, "error creating threads\n", 24);
+			return (false);
+		}
+		threads++;
+	}
+	return (true);
+}
+
 int	run_simu(t_data *data)
 {
 	int	threads;
 
 	threads = 0;
+	//get starttime?
+	//check last eaten time
 	//create threads
 	//join threads
-	while (threads < data->philo_n)
-	{
-		
-	}
-	free(data); //moet ik alles free?
+	free(data); //moet alles freed worden?
 	return (EXIT_SUCCESS);
 }
 
